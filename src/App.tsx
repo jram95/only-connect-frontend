19fabc92dd17e3401683f68f30020a31e6ebd7d2
Jenna <jr13114@my.bristol.ac.uk>
2022-01-24 18:22:24 +0100

    delete greet util files and create components folder and FetchClues component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { greet } from "./utils/greet";
+import FetchClues from "./components/FetchClues";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return <FetchClues />;
 }
 
 export default App;
---
+++ src/components/FetchClues.tsx
@@ -0,0 +1,7 @@
+export default function FetchClues(): JSX.Element {
+  return (
+    <>
+      <h1>Hi</h1>
+    </>
+  );
+}
