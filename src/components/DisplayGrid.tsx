577a2b95ba88976124415d3f310516f81b460298
Jenna <jr13114@my.bristol.ac.uk>
2022-01-25 13:20:14 +0100

    add DisplayGrid component with props passed down from FetchClues

--- src/components/DisplayGrid.tsx
+++ src/components/DisplayGrid.tsx
@@ -1,7 +1,81 @@
-export default function DisplayGrid(): JSX.Element {
+import CluesProps from "./CluesProps";
+import shuffle from "../utils/shuffle";
+
+export default function DisplayGrid({
+  id,
+  submit_time,
+  user,
+  clue11,
+  clue12,
+  clue13,
+  clue14,
+  connection1,
+  explanation1,
+  clue21,
+  clue22,
+  clue23,
+  clue24,
+  connection2,
+  explanation2,
+  clue31,
+  clue32,
+  clue33,
+  clue34,
+  connection3,
+  explanation3,
+  clue41,
+  clue42,
+  clue43,
+  clue44,
+  connection4,
+  explanation4,
+}: CluesProps): JSX.Element {
+  const groups = [
+    {
+      connection: connection1,
+      explanation: explanation1,
+      clues: [clue11, clue12, clue13, clue14],
+    },
+    {
+      connection: connection2,
+      explanation: explanation2,
+      clues: [clue21, clue22, clue23, clue24],
+    },
+    {
+      connection: connection3,
+      explanation: explanation3,
+      clues: [clue31, clue32, clue33, clue34],
+    },
+    {
+      connection: connection4,
+      explanation: explanation4,
+      clues: [clue41, clue42, clue43, clue44],
+    },
+  ];
+
+  const clues = [
+    clue11,
+    clue12,
+    clue13,
+    clue14,
+    clue21,
+    clue22,
+    clue23,
+    clue24,
+    clue31,
+    clue32,
+    clue33,
+    clue34,
+    clue41,
+    clue42,
+    clue43,
+    clue44,
+  ];
+
   return (
     <>
       <h1>Hi</h1>
+      <p>{shuffle(clues)}</p>
     </>
   );
 }
