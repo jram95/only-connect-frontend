989dbac36adfe602d19edca82c0f3b10dd49e8c1
Jenna <jr13114@my.bristol.ac.uk>
2022-01-25 17:39:54 +0100

    removed initial states and put conditional rendering in for <Display Grid />

--- src/components/FetchClues.tsx
+++ src/components/FetchClues.tsx
@@ -7,35 +7,7 @@ const apiBaseURL = process.env.REACT_APP_API_BASE;
 const clue_id = 1;
 
 export default function FetchClues(): JSX.Element {
-  const [wall, getWall] = useState<CluesProps>({
-    id: 0,
-    submit_time: "",
-    user: "",
-    clue11: "",
-    clue12: "",
-    clue13: "",
-    clue14: "",
-    connection1: "",
-    explanation1: "",
-    clue21: "",
-    clue22: "",
-    clue23: "",
-    clue24: "",
-    connection2: "",
-    explanation2: "",
-    clue31: "",
-    clue32: "",
-    clue33: "",
-    clue34: "",
-    connection3: "",
-    explanation3: "",
-    clue41: "",
-    clue42: "",
-    clue43: "",
-    clue44: "",
-    connection4: "",
-    explanation4: "",
-  });
+  const [wall, getWall] = useState<CluesProps>();
   useEffect(() => {
     getClues();
   }, []);
@@ -51,35 +23,37 @@ export default function FetchClues(): JSX.Element {
 
   return (
     <>
-      <DisplayGrid
-        id={wall.id}
-        submit_time={wall.submit_time}
-        user={wall.user}
-        clue11={wall.clue11}
-        clue12={wall.clue12}
-        clue13={wall.clue13}
-        clue14={wall.clue14}
-        connection1={wall.connection1}
-        explanation1={wall.explanation1}
-        clue21={wall.clue21}
-        clue22={wall.clue22}
-        clue23={wall.clue23}
-        clue24={wall.clue24}
-        connection2={wall.connection2}
-        explanation2={wall.explanation2}
-        clue31={wall.clue31}
-        clue32={wall.clue32}
-        clue33={wall.clue33}
-        clue34={wall.clue34}
-        connection3={wall.connection3}
-        explanation3={wall.explanation3}
-        clue41={wall.clue41}
-        clue42={wall.clue42}
-        clue43={wall.clue43}
-        clue44={wall.clue44}
-        connection4={wall.connection4}
-        explanation4={wall.explanation4}
-      />
+      {wall && (
+        <DisplayGrid
+          id={wall.id}
+          submit_time={wall.submit_time}
+          user={wall.user}
+          clue11={wall.clue11}
+          clue12={wall.clue12}
+          clue13={wall.clue13}
+          clue14={wall.clue14}
+          connection1={wall.connection1}
+          explanation1={wall.explanation1}
+          clue21={wall.clue21}
+          clue22={wall.clue22}
+          clue23={wall.clue23}
+          clue24={wall.clue24}
+          connection2={wall.connection2}
+          explanation2={wall.explanation2}
+          clue31={wall.clue31}
+          clue32={wall.clue32}
+          clue33={wall.clue33}
+          clue34={wall.clue34}
+          connection3={wall.connection3}
+          explanation3={wall.explanation3}
+          clue41={wall.clue41}
+          clue42={wall.clue42}
+          clue43={wall.clue43}
+          clue44={wall.clue44}
+          connection4={wall.connection4}
+          explanation4={wall.explanation4}
+        />
+      )}
     </>
   );
 }
