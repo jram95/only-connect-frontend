f73585f804bd1baf428ca54e12b2ed2557969289
Jenna <jr13114@my.bristol.ac.uk>
2022-01-27 12:38:06 +0100

    add ternary operator for lightBlue or darkBlue buttons

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { ButtonsProps } from "./ButtonsProps";
 
 export default function Buttons({
@@ -6,10 +6,10 @@ export default function Buttons({
   setSelectedClues,
   selectedClues,
 }: ButtonsProps): JSX.Element {
-  const [highlightButton, setHighlightButton] = useState<boolean>(false);
+  const darkBlue = "rgb(43, 108, 184)";
+  const lightBlue = "rgb(143, 184, 231)";
 
   function handleClick(clue: string) {
-    setHighlightButton(true);
     if (selectedClues.length === 0) {
       setSelectedClues([clue]);
     } else {
@@ -21,25 +21,41 @@ export default function Buttons({
       <div className="game-container">
         <h1 className="wall-title">Solve the Connecting Wall!</h1>
         <div className="game-grid">
-          {shuffledClues.map((clue) => (
-            <div className="grid-button" key={clue}>
-              <button
-                className="button-clue"
-                onClick={() => handleClick(clue)}
-                style={{
-                  fontSize: "large",
-                  backgroundColor:
-                    highlightButton === true
-                      ? "rgb(43, 108, 184)"
-                      : "rgb(143, 184, 231)",
-                  padding: "20px",
-                  width: "96px",
-                  height: "73px",
-                  border: "1px solid",
-                }}
-              >
-                {clue}
-              </button>
+          {shuffledClues.map((clue, index) => (
+            <div className="grid-button" key={index}>
+              {selectedClues.includes(clue) ? (
+                <button
+                  className="button-clue"
+                  value={clue}
+                  onClick={() => handleClick(clue)}
+                  style={{
+                    fontSize: "large",
+                    backgroundColor: darkBlue,
+                    padding: "20px",
+                    width: "96px",
+                    height: "73px",
+                    border: "1px solid",
+                  }}
+                >
+                  {clue}
+                </button>
+              ) : (
+                <button
+                  className="button-clue"
+                  value={clue}
+                  onClick={() => handleClick(clue)}
+                  style={{
+                    fontSize: "large",
+                    backgroundColor: lightBlue,
+                    padding: "20px",
+                    width: "96px",
+                    height: "73px",
+                    border: "1px solid",
+                  }}
+                >
+                  {clue}
+                </button>
+              )}
             </div>
           ))}
         </div>
