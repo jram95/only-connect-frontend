ab9352be7f16840e537423367ca221499d4fdc55
Jenna <jr13114@my.bristol.ac.uk>
2022-01-27 14:50:34 +0100

    remove remaining clues

--- src/components/SquaresProps.tsx
+++ src/components/SquaresProps.tsx
@@ -3,6 +3,6 @@ import { Dispatch, SetStateAction } from "react";
 export default interface SquaresAndButtonsProps {
   setSelectedClues: Dispatch<SetStateAction<string[]>>;
   selectedClues: string[];
-  remainingClues: string[];
+  shuffledClues: string[];
   correctClues: string[];
 }
