e191707091718cfa52d292a328ed1adfcfc5e370
Jenna <jr13114@my.bristol.ac.uk>
2022-02-07 17:53:23 +0100

    add two more routes for NewWall and IndividuWall

--- src/pages/WebsiteRoutes.tsx
+++ src/pages/WebsiteRoutes.tsx
@@ -6,12 +6,16 @@ import {
   Link,
 } from "react-router-dom";
 import Home from "./Home";
+import IndividuWall from "./IndividuWall";
+import NewWall from "./NewWall";
 
 export default function WebsiteRoutes(): JSX.Element {
   return (
     <>
       <Router>
         <Route path="/" element={<Home />} />
+        <Route path="/connecting-wall" element={<IndividuWall />} />
+        <Route path="/new-wall" element={<NewWall />} />
       </Router>
     </>
   );
