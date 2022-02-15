6e977cf982f977f8e55295c4809baa3cde7df826
Jenna <jr13114@my.bristol.ac.uk>
2022-02-15 12:44:33 +0100

    import InputClues

--- src/components/NewWall/CreateWall.tsx
+++ src/components/NewWall/CreateWall.tsx
@@ -1,8 +1,15 @@
+import InputClues from "./InputClues";
+
 export default function CreateWall(): JSX.Element {
   return (
     <>
       <div className="rules-box">
         <h3>Create your own wall</h3>
+        <p>Create your own connecting wall by filling in the form below:</p>
+        <InputClues />
+        <InputClues />
+        <InputClues />
+        <InputClues />
       </div>
     </>
   );
