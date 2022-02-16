ddaa0d8c7b1f357893cf0cf11a387a81790f26f9
Jenna <jr13114@my.bristol.ac.uk>
2022-02-16 09:53:04 +0100

    create userWall state in parent CreateWall

--- src/components/NewWall/CreateWall.tsx
+++ src/components/NewWall/CreateWall.tsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import InputClues from "./InputClues";
 
 export default function CreateWall(): JSX.Element {
+  const [userWall, setUserWall] = useState([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ]);
+
+  function handleCreateWall() {
+    console.log("hello");
+  }
+
   return (
     <>
       <div className="rules-box">
         <h3>Create your own wall</h3>
-        <p>Create your own connecting wall by filling in the form below:</p>
-        <InputClues />
-        <InputClues />
-        <InputClues />
-        <InputClues />
+        <p>Create your own connecting wall by filling in the form below.</p>
       </div>
+      <InputClues number={1} userWall={userWall} setUserWall={setUserWall} />
+      <InputClues number={2} userWall={userWall} setUserWall={setUserWall} />
+      <InputClues number={3} userWall={userWall} setUserWall={setUserWall} />
+      <InputClues number={4} userWall={userWall} setUserWall={setUserWall} />
+      <button
+        onClick={() => {
+          handleCreateWall();
+        }}
+      >
+        Generate Wall
+      </button>
     </>
   );
 }
