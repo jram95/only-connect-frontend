eb3147c907c2d488a3410131e4ccee6aef48b4e8
Jenna <jr13114@my.bristol.ac.uk>
2022-02-16 13:45:17 +0100

    add fonts, linkStyle and buttonStyle

---
+++ src/components/buttonStyle.js
@@ -0,0 +1,9 @@
+export const buttonStyle = {
+  margin: "1rem",
+  textDecoration: "none",
+  color: "rgb(45, 77, 114)",
+  fontSize: "large",
+  padding: "10px",
+  border: "1px solid",
+  borderRadius: "5px",
+};
